3cf731ae45f1ce0611d6c53d6b5ab92ed64f332e
Ravi Sithiraju <Ravi.Sithiraju@sandstone.com.au>
2025-04-09 17:07:03 +1000

    fixed alignment issues on the home page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { AppService } from '../services/app.service';
 import { Subject } from 'rxjs';
 
@@ -25,6 +25,8 @@ export class ContactUsComponent implements OnDestroy, OnInit {
   @ViewChild('contactUsForm')
   contactUsForm!: ElementRef;
 
+  isSubmitted: boolean = false;
+
   
   constructor(private builder: FormBuilder, private appService: AppService, private cdref: ChangeDetectorRef) {
     this.destroy$ = new Subject<any>();
@@ -36,31 +38,33 @@ export class ContactUsComponent implements OnDestroy, OnInit {
       phone: new FormControl('', []),
       email: new FormControl('', [Validators.email]),
       // comment: new FormControl('', [Validators.required]),
-    });
+    }, { validator: atLeastOne('phone','email') },);
   }
 
   onSubmit(data: any) {
     console.log('fg:: ', this.fg);
     console.log(!data.email && !data.phone);
-    if(!data.email && !data.phone){
-      this.fg?.controls['phone'].addValidators(Validators.required);
-      this.fg?.controls['email'].addValidators(Validators.required);
-    } else {
-      this.fg?.controls['phone'].removeValidators(Validators.required);
-      this.fg?.controls['email'].removeValidators(Validators.required);
-    }
-    this.fg?.controls['phone'].updateValueAndValidity();
-    this.fg?.controls['email'].updateValueAndValidity();
+    // if(!data.email && !data.phone){
+    //   this.fg?.controls['phone'].addValidators(Validators.required);
+    //   this.fg?.controls['email'].addValidators(Validators.required);
+    // } else {
+    //   this.fg?.controls['phone'].removeValidators(Validators.required);
+    //   this.fg?.controls['email'].removeValidators(Validators.required);
+    // }
+    // this.fg?.controls['phone'].updateValueAndValidity();
+    // this.fg?.controls['email'].updateValueAndValidity();
 
-    this.fg?.controls['email'].markAsTouched();
-    this.fg?.controls['phone'].markAsTouched();
-    this.fg?.updateValueAndValidity();
-    this.cdref.detectChanges();
+    // this.fg?.controls['email'].markAsTouched();
+    // this.fg?.controls['phone'].markAsTouched();
+    // this.fg?.updateValueAndValidity();
+    // this.cdref.detectChanges();
     console.log('this.fg.valid ', this.fg?.valid);
+    console.log('this.fg.hasError ', this.fg?.hasError('atLeastOne'));
     console.log('contact-us.component->onSubmit(), data:: ', data);
     if (!this.fg?.valid) {
       this.contactUsForm.nativeElement.classList.add('was-validated');
     } else {
+      this.isSubmitted = true;
       // this.appService.joinWaitingList(data).subscribe();;
       // this.contactUsForm.nativeElement.submit();
     }
@@ -69,4 +73,19 @@ export class ContactUsComponent implements OnDestroy, OnInit {
   ngOnDestroy() {
     this.destroy$.next(true);
   }
+}
+
+export function atLeastOne(...fields: string[]) {
+  return (fg: FormGroup, ): ValidationErrors | null => {
+    return fields.some(fieldName => {
+      const field = fg?.get(fieldName)?.value;
+      // return field && field.length > 0 ? true : false;
+      // if (typeof field === 'string') return field && field.length > 0 ? true : false;
+      if (typeof field === 'number') return field && field >= 0 ? true : false;
+      if (typeof field === 'string') return field && field.length > 0 ? true : false;
+      return true;
+    })
+      ? null
+      : ({ atLeastOne: 'At least one field has to be provided.' } as ValidationErrors);
+  };
 }
